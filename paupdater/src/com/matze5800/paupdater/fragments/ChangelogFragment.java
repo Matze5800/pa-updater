f6d0db98c7f100e95af8c132cb279bc4b3ce80b2
parthipanramesh <parthipan.ramesh@gmail.com>
2013-06-06 14:41:30 +0200

    enabled navigation in webview

--- paupdater/src/com/matze5800/paupdater/fragments/ChangelogFragment.java
+++ paupdater/src/com/matze5800/paupdater/fragments/ChangelogFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 PA Updater (Simon Matzeder and Parthipan Ramesh)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
 package com.matze5800.paupdater.fragments;
 
 import android.app.Fragment;
@@ -33,6 +49,8 @@ public class ChangelogFragment extends Fragment {
 		final WebView webView1 = (WebView) rootView.findViewById(R.id.webView1);
 		WebSettings settings = webView1.getSettings();
 		settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
 		webView1.setWebViewClient(new WebViewClient());
 		if(prefs.getString("Dev", "paranoidandroid").equals("dsmitty166")){
 			webView1.loadUrl("https://dl.dropboxusercontent.com/u/569065/changelog.html");
@@ -40,16 +58,7 @@ public class ChangelogFragment extends Fragment {
             webView1.loadUrl(("https://plus.google.com/app/basic/107979589566958860409/posts"));}
             //URL to old changelog:
             //http://matze5800.de/changelog/"+device
-
-        //Disable navigation and open in the Browser when clicking a link
-		webView1.setOnTouchListener(new OnTouchListener() {
-			@Override
-			public boolean onTouch(View arg0, MotionEvent arg1) {
-				webView1.setWebViewClient(null);
-				return false;
-			}
-        });
-		
+			
 		return rootView;
 	}
 }
